3cac479eac81355139a8038cb995f5ee15f79962
jouaspiazu <joeaspiazu@hotmail.com>
2024-07-04 16:33:49 -0500

    Fixed issues in about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,8 @@
 function About() {
   return (
-    <div className='flex flex-col pt-10 items-center bg-[#6b8a7a] h-screen'>
+    <div className='flex flex-col pt-10 items-center h-screen'>
         <h2 className='text-white'>About Movies</h2>
-        <p className='text-white'>This is a project to show movies in reactjs asdasd</p>
+        <p className='text-white'>This is a project to show movies in reactjs</p>
     </div>
   )
 }
